c16429094636936e7d7b3d23caa3f8c5281f8da2
SrPirson <169342716+SrPirson@users.noreply.github.com>
2025-01-28 11:29:21 +0100

    Clase 28/01/2025

---
+++ 13-ReactBAK/01-Inicio/JS/ejemplo.js
@@ -0,0 +1,19 @@
+/* Instalar React + NPM */
+
+/* 
+    1. Instalar node.js (node --version)
+    2. Instalar npm     (npm --version)
+    3. Actualizar npm   (npm install -g npm@latest)
+    4. Instalar create-react-app (npm install -g create-react-app)
+    5. Actualizar todo (npm update -g)
+    6. Crear la carpeta para el proyecto (mkdir ruta nombre-proyecto)
+    6. Crear proyecto (create-react-app nombre-proyecto)
+
+    Posibles errores:
+    npm config set legacy-peer-deps true
+
+    Cannot find module 'ajv/dist/compile/codegen'
+    error occured, then
+
+    npm install --save-dev ajv@^8
+*/
