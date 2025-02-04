b801683283c43b6dd2d4202ba921fa2b3fcf1178
SrPirson <169342716+SrPirson@users.noreply.github.com>
2025-02-04 11:17:40 +0100

    Clase 04/02/2025

--- 12-react/src/App.js
+++ 12-react/src/App.js
@@ -3,12 +3,45 @@ import React,{useRef, useState} from 'react'; // libreria para hacer referencias
 import Iterador from './iterador'; // importamos el componente iterador
 
 function App() {
+  const [numero, setNumero] = useState(); // declaramos el estado numero
+  const resultado = useRef();
+  const [primerCampo, setPrimerCampo] = useState("");
+  const [segundoCampo, setSegundoCampo] = useState("");
+
+  const seleccionar = (e) => {
+    setNumero(e.target.value); // asignamos el valor del select al estado numero
+  }
+
+  const modificar1 = (e) => {
+    setPrimerCampo = e.target.value;
+  }
+
+  const modificar2 = (e) => {
+    setSegundoCampo = e.target.value;
+  }
+
+  const sumar = () => {
+    resultado.current.value = parseInt(primerCampo) + parseInt(segundoCampo);
+  }
   
   return (
     <div>
-      <Iterador/> {/* usamos el iterador.js */}
-      <Iterador/>
-      <Iterador/>
+      <Iterador/><br/> {/* usamos el iterador.js */}
+
+      <select value={numero} onChange={seleccionar}>
+        <option value="" hidden>Selecciona un número</option>
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <p>La opción seleccionada es: {numero}</p>
+
+
+      <input type="number" value={primerCampo} onChange={modificar1} /><span> + </span>
+      <input type="number" value={segundoCampo} onChange={modificar2} /><span> = </span>
+      <input ref={resultado} readOnly />
+      <button onClick={sumar}>Sumar</button>
     </div>
     
   );
